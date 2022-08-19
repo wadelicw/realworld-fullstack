00d0689e5469a8ce5c96e7d63af1debb624fd105
Wade Li <wade.li@eforcity.com>
2022-08-19 15:42:25 +0800

    Profile page initial

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,31 +1,68 @@
 import React from "react";
+import classnames from "classnames";
+import { withRouter } from "next/router";
 
+@withRouter
 class Navbar extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			path: this.props.router.pathname
+		};
+	}
+
 	render() {
+
+		const { path } = this.state;
+
 		return (
 			<nav className="navbar navbar-light">
 				<div className="container">
 					<a className="navbar-brand" href="/">conduit</a>
 					<ul className="nav navbar-nav pull-xs-right">
 						<li className="nav-item">
-							<a className="nav-link active" href="/">Home</a>
+							<a
+								className={classnames(
+									"nav-link",
+									path === "/" ? "active" : undefined
+								)}
+								href="/"
+							>
+								Home
+							</a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="">
 								<i className="ion-compose"></i>&nbsp;New Article
-                        </a>
+                        	</a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="">
 								<i className="ion-gear-a"></i>&nbsp;Settings
-                        </a>
+                        	</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="/login">Sign in</a>
+							<a
+								className={classnames(
+									"nav-link",
+									path === "/login" ? "active" : undefined
+								)}
+								href="/login"
+							>
+								Sign in
+							</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="/register">Sign up</a>
+							<a
+								className={classnames(
+									"nav-link",
+									path === "/register" ? "active" : undefined
+								)}
+								href="/register"
+							>
+								Sign up
+							</a>
 						</li>
 					</ul>
 				</div>
---
+++ frontend/pages/profile/[username].js
@@ -0,0 +1,3 @@
+import Page from "../../pages.components/profile";
+
+export default Page;
