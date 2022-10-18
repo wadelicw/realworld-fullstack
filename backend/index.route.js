4012203242ae1c68f52aac78187bda6b4f9ff4ea
Wade Li <wade.li@eforcity.com>
2022-10-18 17:22:41 +0800

    Update api docs

--- backend/index.route.js
+++ backend/index.route.js
@@ -15,8 +15,8 @@ router.use("/tag", require("./tag/tag.route"));
 router.use("/profile", require("./profile/profile.route"));
 router.use("/article", require("./article/article.route"));
 
-//For API document
-router.use("/docs", require("./docs/docs.route"))
+//	For API document
+router.use("/docs", require("./docs/docs.route"));
 
 router.use((error, req, res, next) => {
 	// customize Joi validation errors
